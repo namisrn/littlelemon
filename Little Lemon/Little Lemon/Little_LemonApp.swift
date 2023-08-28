7b45456123990f1b03f91b5cacaa8b6089698128
Sasan Rafat Nami <Nami.srn@gmail.com>
2023-08-28 06:34:14 +0200

    init little Lemon

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Sasan Rafat Nami on 28.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
